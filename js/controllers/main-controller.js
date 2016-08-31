9c0f8b16d6f9cb4a6584b479c42d14990947df61
Matheus Oliveira Tristão dos Anjos <matheusanjos@unasus.gov.br>
2016-08-31 14:46:01 -0300

    display none if cards are equal

--- js/controllers/main-controller.js
+++ js/controllers/main-controller.js
@@ -6,126 +6,126 @@ angular.module('memory').controller('MainController', function ($scope) {
             name: "eigth",
             url: "images/eigth.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 2,
             name: "tennis",
             url: "images/tennis.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 3,
             name: "football",
             url: "images/football.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 4,
             name: "white",
             url: "images/white.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 5,
             name: "bow",
             url: "images/bow.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 6,
             name: "volley",
             url: "images/volley.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 7,
             name: "basket",
             url: "images/basket.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 8,
             name: "baseball",
             url: "images/baseball.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 9,
             name: "golf",
             url: "images/golf.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 10,
             name: "eigth",
             url: "images/eigth.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 11,
             name: "tennis",
             url: "images/tennis.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 12,
             name: "football",
             url: "images/football.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 13,
             name: "white",
             url: "images/white.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 14,
             name: "bow",
             url: "images/bow.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 16,
             name: "volley",
             url: "images/volley.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 16,
             name: "basket",
             url: "images/basket.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 17,
             name: "baseball",
             url: "images/baseball.png",
             show: "hide",
-            display: true
+            display: "true"
         },
         {
             id: 18,
             name: "golf",
             url: "images/golf.png",
             show: "hide",
-            display: true
+            display: "true"
         }
     ];
 
@@ -146,11 +146,20 @@ angular.module('memory').controller('MainController', function ($scope) {
                 closeAll(cards);
                 count = 1;
             }
-            card.show = "show";
-            card.display = false;
-        }
 
-        alert(count);
+            for (var i = 0; i < cards.length; i++) {
+                if (cards[i].name === card.name) {
+                    if (cards[i].show === "show") {
+                        card.show = "show";
+                        card.display = "false";
+                        cards[i].display = "false";
+                    }
+                }
+            }
+
+            card.show = "show";
+
+        }
     };
 
     function closeAll(cards) {
